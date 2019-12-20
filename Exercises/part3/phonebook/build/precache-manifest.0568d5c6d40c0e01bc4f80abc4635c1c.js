8c008399e2c6c9289586dd01dfbc471d25744ca8
do-phuong <phuong@dactech.vn>
2019-12-20 14:46:57 +0700

    deploy code to heroku

---
+++ Exercises/part3/phonebook/build/precache-manifest.0568d5c6d40c0e01bc4f80abc4635c1c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3d7c6019401c98ffae35283254c9642",
+    "url": "/index.html"
+  },
+  {
+    "revision": "339689c50685355aca19",
+    "url": "/static/css/main.80bcf384.chunk.css"
+  },
+  {
+    "revision": "4cb06bb313047e414d0e",
+    "url": "/static/js/2.edce1816.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/static/js/2.edce1816.chunk.js.LICENSE"
+  },
+  {
+    "revision": "339689c50685355aca19",
+    "url": "/static/js/main.3fae996c.chunk.js"
+  },
+  {
+    "revision": "7b5fb82eec8b254ec5fc",
+    "url": "/static/js/runtime-main.eea8aeb0.js"
+  }
+]);
--- Exercises/part3/phonebook/index.js
+++ Exercises/part3/phonebook/index.js
@@ -2,7 +2,10 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 var morgan = require('morgan')
+const cors = require('cors')
 
+app.use(express.static('build'))
+app.use(cors())
 app.use(bodyParser.json())
 morgan.token('body', function (req) { return JSON.stringify(req.body) })
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
@@ -90,7 +93,7 @@ app.post('/api/persons', (req, res) => {
   res.json(person)
 })
 
-const port = 3001
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`)
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
 })
