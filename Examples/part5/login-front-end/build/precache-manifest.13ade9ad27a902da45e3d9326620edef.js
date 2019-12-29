780613ff1125aba06466d99d4aa5f44d2d5dbef6
at-phuongdo <dophuong13t2@gmail.com>
2019-12-29 22:27:36 +0700

    Example part 5a Login frontend

---
+++ Examples/part5/login-front-end/build/precache-manifest.13ade9ad27a902da45e3d9326620edef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84626389ae4521a1737fe3bf60e90e72",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a23ad8fedfa1ae09d0f1",
+    "url": "/static/css/main.6b25e63b.chunk.css"
+  },
+  {
+    "revision": "1bc0c6c41913c67f9e72",
+    "url": "/static/js/2.44a42e06.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/static/js/2.44a42e06.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a23ad8fedfa1ae09d0f1",
+    "url": "/static/js/main.b2391655.chunk.js"
+  },
+  {
+    "revision": "dfeb9fbe364051343b1b",
+    "url": "/static/js/runtime-main.8e8cbd7f.js"
+  }
+]);
