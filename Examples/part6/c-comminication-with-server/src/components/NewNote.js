8af9f57e446dccb5bc7ba5d93ab19910b276b52b
at-phuongdo <dophuong13t2@gmail.com>
2020-01-08 23:35:41 +0700

    Example part 6c

---
+++ Examples/part6/c-comminication-with-server/src/components/NewNote.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import { connect } from 'react-redux'
+import { createNote } from '../reducers/noteReducer'
+import noteService from '../services/notes'
+
+const NewNote = (props) => {
+  const addNote = async (event) => {
+    event.preventDefault()
+    const content = event.target.note.value
+    event.target.note.value = ''
+    const newNote = await noteService.createNew(content)
+    props.createNote(newNote)
+  }
+
+  return (
+    <form onSubmit={addNote}>
+      <input name="note" />
+      <button type="submit">add</button>
+    </form>
+  )
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    createNote: value => {
+      dispatch(createNote(value))
+    },
+  }
+}
+
+export default connect(
+  null,
+  mapDispatchToProps
+)(NewNote)
